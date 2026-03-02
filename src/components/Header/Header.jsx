398207886e9afbcbd848a43312820cdc7523c167
kazak <kazakova.katya@outlook.com>
2026-03-02 21:15:44 +0700

    refactor header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {motion} from "motion/react"
+
+export const Header = ( props ) => {
+    return (
+        <motion.div
+            className={"header-container"}
+            initial={{ opacity: 0.2, y: -10 }}
+            animate={{ opacity: 1,  y: 0 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+        >
+            <div className={"detail-container"}>
+                <h1> To-do List </h1>
+                <p style={{marginBottom: "0.5rem"}}> Open tasks: {props.tasks ? props.tasks.length : 0} </p>
+                <motion.button
+                    className={"create-button"}
+                    onClick={props.handleAddTask}
+                    whileHover={{scale: 1.05}}
+                    whileTap={{scale: 1}}
+                    transition={{type: "spring", stiffness: 300, damping: 20}}
+                >
+                    Add new task
+                </motion.button>
+            </div>
+
+            {/* colour picker */}
+            {props.focusTaskId && (
+                <div className={'task-customization'} onPointerDown={(e) => e.stopPropagation()}>
+                    <div className={'colour-palette'}>
+                        {props.colours.map((colour, index) =>
+                            <motion.button
+                                key={index}
+                                className={'colour-choice'}
+                                onClick={() => props.changeTaskColour(props.focusTaskId, colour)}
+                                style={{background: colour}}
+
+                                whileHover={{scale: 1.1, border: "#fff solid 2px" }}
+                                transition={{type: "spring", stiffness: 300, damping: 20,
+                                    border: {duration: 0}, }
+                                }
+                            />
+                        )}
+                    </div>
+                    <input
+                        className={'colour-picker'}
+                        type={'color'}
+                        value={props.focusTask.colour.split(" ")[2]}
+                        onChange={(e) => props.changeTaskColour(props.focusTaskId, e.target.value)}
+                        onPointerDown={(e) => e.stopPropagation()}
+                    />
+                </div>
+            )}
+        </motion.div>
+    )
+}
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,73 @@
+import './homepage.css'
+import '../../components/Header/header.css'
+import React, {useRef} from "react";
+import {Board} from "../../components/Board/Board.jsx";
+import {useTasks} from "../../hooks/useTasks.js";
+import {useColours} from "../../hooks/useColours.js";
+import {Header} from "../../components/Header/Header.jsx";
+
+function Homepage() {
+
+    const boardReference = useRef(null)
+
+    const {
+        tasks,
+        focusTaskId, setFocusTaskId,
+        focusTask,
+        addTask,
+        deleteTask,
+        updateTaskLocation,
+        resizeTask,
+        updateTaskContent,
+        changeTaskColour,
+        bringTaskToFront
+    } = useTasks()
+
+    const {colours, addColour, deleteColour} = useColours()
+
+    const handleAddTask = () => {
+        const task_w = 150
+        const task_h = 150
+        const defaultColour = colours[10]
+        let x = 0
+        let y = 0
+
+        if (boardReference.current) {
+            const rect = boardReference.current.getBoundingClientRect()
+            x = (rect.width / 2) - (task_w / 2)
+            y = (rect.height / 2) - (task_h / 2)
+        }
+
+        addTask({ x, y }, defaultColour)
+    }
+
+    return (
+        <div className={"homepage-container"} onPointerDown={() => setFocusTaskId(null)}>
+
+            {/* menu at the top */}
+            <Header
+                tasks={tasks}
+                handleAddTask={handleAddTask}
+                colours={colours}
+                changeTaskColour={changeTaskColour}
+                focusTaskId={focusTaskId}
+                focusTask={focusTask}
+            />
+
+            <div className={"board-container"}>
+                <Board
+                    tasks={tasks}
+                    deleteFunction={deleteTask}
+                    updateLocation={updateTaskLocation}
+                    boardRef={boardReference}
+                    resize={resizeTask}
+                    updateTaskContent={updateTaskContent}
+                    bringToFront={bringTaskToFront}
+                    focusTaskId={focusTaskId}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
